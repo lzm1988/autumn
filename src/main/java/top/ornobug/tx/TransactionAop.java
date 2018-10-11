d46edafe144055dd92ce4c268fbd50935923c8bf
lzm1988! <myworld20151124@gmail.com>
2018-10-11 16:21:43 +0800

    事务注解

--- src/main/java/top/ornobug/tx/TransactionAop.java
+++ src/main/java/top/ornobug/tx/TransactionAop.java
@@ -1,35 +1,6 @@
 package top.ornobug.tx;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-
-@Aspect
 public class TransactionAop {
 
-    static {
-        System.out.println(1);
-    }
 
-    @Pointcut(value = "@annotation(top.ornobug.annotation.Transaction)")
-    public void pointCut() {
-
-    }
-
-    @Before(value = "pointCut()")
-    public void before(JoinPoint joinPoint) {
-        System.out.println("切点开始");
-        System.out.println(joinPoint.getSignature());
-
-    }
-
-
-    @After(value = "pointCut()")
-    public void after(JoinPoint joinPoint) {
-        System.out.println(joinPoint.getSignature());
-        System.out.println("切点结束");
-
-    }
 }
